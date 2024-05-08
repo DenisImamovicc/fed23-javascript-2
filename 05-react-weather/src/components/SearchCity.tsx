7af0977b810a61e873760ab997e4f80ab197bc5f
Denis Imamovic <Danielarbete@protonmail.com>
2024-05-08 12:01:08 +0200

    Add user being able to send city to api and get data with err handling.

--- 05-react-weather/src/components/SearchCity.tsx
+++ 05-react-weather/src/components/SearchCity.tsx
@@ -1,23 +1,47 @@
-const SearchCity = () => {
-	return (
-		<div id="search-wrapper">
-			<form id="search-form">
-				<div className="input-group">
-					<input
-						type="text"
-						className="form-control"
-						placeholder="Enter city to search for"
-						aria-label="City"
-						aria-details="Search for city to show current weather for."
-					/>
+import { useState } from 'react';
+import { getCurrentWeather } from "../services/OWMAPI";
 
-					<button type="submit" className="btn btn-success">
-						🔍
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+const SearchCity = () => {
+    const [city, setCity] = useState("");
+
+    const handleSearchFormSubmit = async(e:React.FormEvent) => {
+        e.preventDefault();
+        let data;
+        
+		try {
+			data = await getCurrentWeather(city);
+		} catch (error) {
+			alert("City not FOUND!")
+		}
+        console.log(city,data);
+		setCity("")
+    };
+
+    const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setCity(e.target.value);
+    };
+
+    return (
+        <div id="search-wrapper">
+            <form id="search-form" onSubmit={handleSearchFormSubmit}>
+                <div className="input-group">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter city to search for"
+                        aria-label="City"
+                        aria-details="Search for city to show current weather for."
+                        value={city} 
+                        onChange={handleInputChange} 
+                    />
+
+                    <button type="submit" className="btn btn-success">
+                        🔍
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default SearchCity;
